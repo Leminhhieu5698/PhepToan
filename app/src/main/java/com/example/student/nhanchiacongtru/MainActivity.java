2c18a909f08d3acd7d5e6b0441780e55c790805b
le Minh Hieu <Lhdesign56@gmail.com>
2019-02-20 08:39:26 +0700

    Them nut cong

--- app/src/main/java/com/example/student/nhanchiacongtru/MainActivity.java
+++ app/src/main/java/com/example/student/nhanchiacongtru/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.student.nhanchiacongtru;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText so_x,so_y;
+    TextView kq;
+    Button cong,tru,nhan,chia;
+    float x,y,z;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       
     }
 }
